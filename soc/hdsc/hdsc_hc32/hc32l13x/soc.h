538458a21a6ee1c5b858376acc7c8b8f934c9771
Michael Zimmermann <sigmaepsilon92@gmail.com>
2024-09-30 21:29:00 +0200

    Add hc32l13x SoC

---
+++ soc/hdsc/hdsc_hc32/hc32l13x/soc.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SOC_HDSC_HC32L13X_H_
+#define SOC_HDSC_HC32L13X_H_
+
+#include <cmsis_core_m_defaults.h>
+
+#endif /* SOC_HDSC_HC32L13X_H_ */
+
