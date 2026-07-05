cde827273f23af8a63ba0b8f4fbf8337f00fdf00
Michael Zimmermann <sigmaepsilon92@gmail.com>
2026-07-05 18:42:51 +0200

    WIP: net: mqtt_sn: Don't remove manually added gateways
    Other parts of the code already make sure to not act on gateways with
    adv_timer==-1. The ping timeout code path didn't.
    
    I've also added a check to make sure gw is not NULL, just in case the list
    is empty for some weird reason.

--- subsys/net/lib/mqtt_sn/mqtt_sn.c
+++ subsys/net/lib/mqtt_sn/mqtt_sn.c
@@ -1009,8 +1009,10 @@ static int process_ping(struct mqtt_sn_client *client, int64_t *next_cycle)
 			LOG_WRN("Ping ran out of retries");
 			mqtt_sn_disconnect_internal(client);
 			gw = SYS_SLIST_PEEK_HEAD_CONTAINER(&client->gateways, gw, next);
-			LOG_DBG("Removing non-responsive GW 0x%02x", gw->gw_id);
-			mqtt_sn_gw_destroy(client, gw);
+			if (gw && gw->adv_timer != -1) {
+				LOG_DBG("Removing non-responsive GW 0x%02x", gw->gw_id);
+				mqtt_sn_gw_destroy(client, gw);
+			}
 			return -ETIMEDOUT;
 		}
 
